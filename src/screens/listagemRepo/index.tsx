ef54ffcb207401b114529eb76a91f6949efd8da5
techjuliana <julianasouzabittencourt@gmail.com>
2023-02-28 10:56:32 -0300

    estilo

--- src/screens/listagemRepo/index.tsx
+++ src/screens/listagemRepo/index.tsx
@@ -56,6 +56,9 @@ export function ListagemRepo() {
         placeholder="Pesquise um repositorio..."
         onChangeText={(text) => searchFilter(text)}
         value={search}
+        returnKeyType="send"
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Container>
         <FlatList
--- src/screens/listagemUsers/index.tsx
+++ src/screens/listagemUsers/index.tsx
@@ -2,19 +2,25 @@ import { Container } from "./styles";
 import React, { useEffect, useState } from "react";
 import { Users } from "../../components/users";
 import api from "../../services/api";
-import {  FlatList } from "react-native";
+import {  FlatList, Text } from "react-native";
 import { UserDTO } from "../../dtos/UserDTO";
 import { Input } from "../../components/Input";
 import { HeaderUser } from "../../components/headerUser";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 export function ListagemUsers() {
   const [usersDados, setUsersDados] = useState<UserDTO[]>([]);
+  const [inputText, setInputText] = useState(null);
+
+  function Add (){
+
+  }
 
   console.log("teste api funcionando", usersDados);
 
   useEffect(() => {
     async function loadUsers() {
-      const response = await api.get("users/techjuliana");
+      const response = await api.get(`users/${inputText}`);
       setUsersDados([response.data]);
     }
 
@@ -24,7 +30,16 @@ export function ListagemUsers() {
     return (
       <Container>
         <HeaderUser />
-        <Input placeholder="Pesquise seu usuario..." />
+        <Input placeholder="Pesquise seu usuario..." 
+        onChangeText={setInputText}
+        returnKeyType="send"
+        autoCapitalize="none"
+        autoCorrect={false}
+        // onSubmitEditing={Add}
+        />
+        {/* <ButtonIcon onPress={Add}>
+          <Text>enviar</Text>
+        </ButtonIcon> */}
         <Container>
           <FlatList
             data={usersDados}
